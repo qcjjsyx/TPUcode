70fc2ce3cf1f10bf60f11809656cf9836113b9ac
黄渊 <17757924971@163.com>
2025-11-25 19:18:29 +0800

    2

---
+++ asc_part/cMutexMerge9_d_streamInst.v
@@ -0,0 +1,203 @@
+//===============================================================================
+// Project:        RCA
+// Module:         ConfMerge_2_d
+// Author:         YiHua Lu
+// Date:           2025/06/18
+// Description:    互斥融合;带数据版;对数据不做持久化保存；
+//                 在控制链加入数据的模型中，mutex的实现逻辑是导致数据非持久化的原因
+//                 测试值：1ns左右可出去
+//===============================================================================
+//! 收到i_free后再发o_free
+
+`timescale 1ns / 1ps
+
+module cMutexMerge9_d_streamInst#(
+    parameter DATA_WIDTH = 128
+)(
+    // in0 -->
+    (* dont_touch="true" *)input                    i_drive0    ,
+    (* dont_touch="true" *)output                   o_free0     , 
+    (* dont_touch="true" *)input  [DATA_WIDTH-1:0]  i_data0,
+    // in1 -->
+    (* dont_touch="true" *)input                    i_drive1    ,
+    (* dont_touch="true" *)output                   o_free1     ,
+    (* dont_touch="true" *)input  [DATA_WIDTH-1:0]  i_data1,
+    // in2 -->
+    (* dont_touch="true" *)input                    i_drive2    ,
+    (* dont_touch="true" *)output                   o_free2     ,
+    (* dont_touch="true" *)input  [DATA_WIDTH-1:0]  i_data2,
+    // in3 -->
+    (* dont_touch="true" *)input                    i_drive3    ,
+    (* dont_touch="true" *)output                   o_free3     ,
+    (* dont_touch="true" *)input  [DATA_WIDTH-1:0]  i_data3,
+    // in4 -->
+    (* dont_touch="true" *)input                    i_drive4    ,
+    (* dont_touch="true" *)output                   o_free4     ,
+    (* dont_touch="true" *)input  [DATA_WIDTH-1:0]  i_data4,
+    // in5 -->
+    (* dont_touch="true" *)input                    i_drive5    ,
+    (* dont_touch="true" *)output                   o_free5     ,
+    (* dont_touch="true" *)input  [DATA_WIDTH-1:0]  i_data5,
+    // in6 -->
+    (* dont_touch="true" *)input                    i_drive6    ,
+    (* dont_touch="true" *)output                   o_free6     ,
+    (* dont_touch="true" *)input  [DATA_WIDTH-1:0]  i_data6,
+    // in7 -->
+    (* dont_touch="true" *)input                    i_drive7    ,
+    (* dont_touch="true" *)output                   o_free7     ,
+    (* dont_touch="true" *)input  [DATA_WIDTH-1:0]  i_data7,
+    // in8 -->
+    (* dont_touch="true" *)input                    i_drive8    ,
+    (* dont_touch="true" *)output                   o_free8     ,
+    (* dont_touch="true" *)input  [DATA_WIDTH-1:0]  i_data8,
+
+
+
+
+    // --> out
+    (* dont_touch="true" *)output                   o_driveNext ,
+    (* dont_touch="true" *)input                    i_freeNext  ,
+    (* dont_touch="true" *)output [DATA_WIDTH-1:0]  o_data,
+
+    (* dont_touch="true" *)input                    rst
+);
+
+(* dont_touch="true" *)wire        w_firstTrig     , w_secondTrig, w_thirdTrig,
+                                w_fourthTrig    , w_fifthTrig,
+                                w_sixthTrig     , w_seventhTrig,
+                                w_eighthTrig    , w_ninthTrig;
+(* dont_touch="true" *)wire        w_firstReq      , w_secondReq, w_thirdReq,
+                                w_fourthReq     , w_fifthReq,
+                                w_sixthReq      , w_seventhReq,
+                                w_eighthReq     , w_ninthReq;
+
+(* dont_touch="true" *)wire        w_free0         , w_free1, w_free2,
+                                w_free3         , w_free4,
+                                w_free5         , w_free6,
+                                w_free7         , w_free8;
+(* dont_touch="true" *)wire        w_free0_delay   , w_free1_delay,w_free2_delay,
+                                w_free3_delay   , w_free4_delay,
+                                w_free5_delay   , w_free6_delay,
+                                w_free7_delay   , w_free8_delay;
+
+
+(* dont_touch="true" *)wire [DATA_WIDTH-1:0] w_data;
+
+delay1U delay0(.inR(w_free0), .outR(w_free0_delay), .rst(rst));
+assign w_firstTrig = i_drive0&(~w_firstReq) | w_free0_delay&(w_firstReq);
+
+contTap firstTap(
+    .trig       ( w_firstTrig   ),
+    .req        ( w_firstReq    ),
+    .rst        ( rst           )
+);
+
+delay1U delay1(.inR(w_free1), .outR(w_free1_delay), .rst(rst));
+assign w_secondTrig = i_drive1&(~w_secondReq) | w_free1_delay&(w_secondReq);
+
+contTap secondTap(
+    .trig       ( w_secondTrig  ),
+    .req        ( w_secondReq   ),
+    .rst        ( rst           )
+);
+
+delay1U delay2(.inR(w_free2), .outR(w_free2_delay), .rst(rst));
+assign w_thirdTrig = i_drive2&(~w_thirdReq) | w_free2_delay&(w_thirdReq);
+contTap thirdTap(
+    .trig       ( w_thirdTrig   ),
+    .req        ( w_thirdReq    ),
+    .rst        ( rst           )
+);
+
+delay1U delay3(.inR(w_free3), .outR(w_free3_delay), .rst(rst));
+assign w_fourthTrig = i_drive3&(~w_fourthReq) | w_free3_delay&(w_fourthReq);
+contTap fourthTap(  
+    .trig       ( w_fourthTrig  ),
+    .req        ( w_fourthReq   ),
+    .rst        ( rst           )
+); 
+
+delay1U delay4(.inR(w_free4), .outR(w_free4_delay), .rst(rst));
+assign w_fifthTrig = i_drive4&(~w_fifthReq) | w_free4_delay&(w_fifthReq);
+contTap fifthTap(  
+    .trig       ( w_fifthTrig   ),
+    .req        ( w_fifthReq    ),
+    .rst        ( rst           )
+);      
+
+delay1U delay5(.inR(w_free5), .outR(w_free5_delay), .rst(rst));
+assign w_sixthTrig = i_drive5&(~w_sixthReq) | w_free5_delay&(w_sixthReq);
+contTap sixthTap(  
+    .trig       ( w_sixthTrig   ),
+    .req        ( w_sixthReq    ),
+    .rst        ( rst           )
+);      
+
+delay1U delay6(.inR(w_free6), .outR(w_free6_delay), .rst(rst));
+assign w_seventhTrig = i_drive6&(~w_seventhReq) | w_free6_delay&(w_seventhReq);
+contTap seventhTap( 
+    .trig       ( w_seventhTrig ),
+    .req        ( w_seventhReq  ),
+    .rst        ( rst           )
+);      
+
+
+delay1U delay7(.inR(w_free7), .outR(w_free7_delay), .rst(rst));
+assign w_eighthTrig = i_drive7&(~w_eighthReq) | w_free7_delay&(w_eighthReq);
+contTap eighthTap(  
+    .trig       ( w_eighthTrig  ),
+    .req        ( w_eighthReq   ),
+    .rst        ( rst           )
+);
+
+delay1U delay8(.inR(w_free8), .outR(w_free8_delay), .rst(rst));
+assign w_ninthTrig = i_drive8&(~w_ninthReq) | w_free8_delay&(w_ninthReq);
+contTap ninthTap(  
+    .trig       ( w_ninthTrig   ),
+    .req        ( w_ninthReq    ),
+    .rst        ( rst           )
+);
+
+
+
+
+
+// 数据需要一定的时间被赋值，这里让o_drive出去前数据已经准备好
+(* dont_touch="true" *)wire w_driveNext = i_drive0 | i_drive1 | i_drive2 | i_drive3 | i_drive4
+                                | i_drive5 | i_drive6 | i_drive7 | i_drive8;
+delay1U delay_out(.inR(w_driveNext), .outR(o_driveNext), .rst(rst));
+
+assign w_free0 = i_freeNext & w_firstReq;
+assign w_free1 = i_freeNext & w_secondReq;
+assign w_free2 = i_freeNext & w_thirdReq;
+assign w_free3 = i_freeNext & w_fourthReq;
+assign w_free4 = i_freeNext & w_fifthReq;
+assign w_free5 = i_freeNext & w_sixthReq;
+assign w_free6 = i_freeNext & w_seventhReq;
+assign w_free7 = i_freeNext & w_eighthReq;
+assign w_free8 = i_freeNext & w_ninthReq;
+
+assign o_free0 = w_free0;
+assign o_free1 = w_free1;
+assign o_free2 = w_free2;
+assign o_free3 = w_free3;
+assign o_free4 = w_free4;
+assign o_free5 = w_free5;
+assign o_free6 = w_free6;
+assign o_free7 = w_free7;
+assign o_free8 = w_free8;
+
+
+assign w_data =  (w_firstReq  == 1'b1) ? i_data0 :
+		    	((w_secondReq == 1'b1) ? i_data1 :
+                ((w_thirdReq  == 1'b1) ? i_data2 :
+                ((w_fourthReq == 1'b1) ? i_data3 :
+                ((w_fifthReq  == 1'b1) ? i_data4 :
+                ((w_sixthReq  == 1'b1) ? i_data5 :
+                ((w_seventhReq== 1'b1) ? i_data6 :
+                ((w_eighthReq == 1'b1) ? i_data7 :
+                ((w_ninthReq  == 1'b1) ? i_data8 : {DATA_WIDTH{1'b0}})))))))));
+
+assign o_data = w_data;
+
+endmodule
--- asc_part/stream_inst.v
+++ asc_part/stream_inst.v
@@ -7,70 +7,65 @@ module stream_inst(
 
     // BRAM READ Interface
     output RD_START,
-    output[31:0] RD_ADDR,
-    input[127:0] RD_DATA,
+    output [31:0] RD_ADDR,
+    input [127:0] RD_DATA,
     input RD_DONE,
+
 
     // TPU Interface
     output o_drive2TPU,
     input  i_freeFTPU,
-    output[127:0] o_streamInst_data_128,
+    output[127:0] o_data2TPU_128,
     output o_finish2TPU,
 
     input i_driveFTPU,
-    output o_freeFTPU,
-    input[127:0] i_TPU_data_128,
+    output o_free2TPU,
+    input[127:0] i_dataFTPU_128,
     input i_finishFTPU,
 
     input rst
 
 );
 
-wire w_readInst_fire, w_readInst_fire_delay;
 
 
-cFifo1_user read_inst_fifo(
+wire w_drive_readStreamInst, w_free_readStreamInst;
+
+cFifo1_user read_StreamInst_fifo(
     .i_drive     ( CAN_READ_INST     ),
     .o_free      (       ),
-    .o_driveNext (  ),
-    .i_freeNext  ( w_readInst_fire_delay  ),
-    .o_fire      ( w_readInst_fire     ), 
+    .o_driveNext ( w_drive_readStreamInst ),
+    .i_freeNext  ( w_free_readStreamInst ),
+    .o_fire      (     ), 
     .rst         ( rst         )
 );
-delay2U readInst_delay2U(
-    .inR  ( w_readInst_fire  ),
-    .outR ( w_readInst_fire_delay ),
-    .rst  ( rst  )
-);
-// 读出指令
-always @(posedge w_readInst_fire or posedge RD_DONE or negedge rst) begin
-    if (!rst) begin
-        RD_START_REG <= 1'b0;
-        RD_ADDR_REG  <= 32'b0;
-    end else if (RD_DONE) begin
-        RD_START_REG <= 1'b0;
-    end else begin
-        RD_START_REG <= 1'b1;
-        RD_ADDR_REG  <= 32'hC000_0000; // 假设指令地址为C000_0000
-    end
-end
+
+
+
+
+
+
+
+
 
 // 锁存指令
-wire w_inst_fire;
+wire w_drive_streamInstReadDone;
+wire w_free_streamInstReadDone;
+wire w_fire_streamInstReadDone;
 wire w_drive_analyzeInst;
-wire w_freeFAnalyzeInst;
+wire w_free_analyzeInst;
 reg [127:0] INST_REG;
 reg[7:0] OP_REG;
-cFifo1_user read_inst_fifo(
-    .i_drive     ( RD_DONE     ),
-    .o_free      (       ),
+cFifo1_user read_Inst_fifo(
+    .i_drive     ( w_drive_streamInstReadDone ),
+    .o_free      ( w_free_streamInstReadDone ),
     .o_driveNext ( w_drive_analyzeInst ),
-    .i_freeNext  ( w_freeFAnalyzeInst ),
-    .o_fire      ( w_inst_fire     ), 
+    .i_freeNext  ( w_free_analyzeInst ),
+    .o_fire      ( w_fire_streamInstReadDone     ), 
     .rst         ( rst         )
 );
 
-always @(posedge w_inst_fire or negedge rst) begin
+always @(posedge w_fire_streamInstReadDone or negedge rst) begin
     if (!rst) begin
         INST_REG <= 128'b0;
         OP_REG   <= 8'b0;
@@ -79,6 +74,8 @@ always @(posedge w_inst_fire or negedge rst) begin
         OP_REG <=RD_DATA[127:120];
     end
 end
+
+
 
 wire w_valid0_streamInst, w_valid1_streamInst, w_valid2_streamInst, w_valid3_streamInst, w_valid4_streamInst;
 assign w_valid0_streamInst = (OP_REG == 8'h10); // 发送指令
@@ -89,7 +86,7 @@ assign w_valid4_streamInst = (OP_REG == 8'h14); // 获取状态
 wire w_drive2_SEND_Inst, w_drive2_SEND_DATA,w_drive2_GET_DATA,w_drive2_FREE,w_drive2_GET_STATUS;
 wire w_freeF_SEND_Inst, w_freeF_SEND_DATA,w_freeF_GET_DATA,w_freeF_FREE,w_freeF_GET_STATUS;
 cSelSplit5_streamInst u_cSelSplit5_streamInst(
-    .i_drive      ( w_drive_analyzeInst      ),
+    .i_drive      ( w_drive_analyzeInst     ),
     .i_freeNext0  ( w_freeF_SEND_Inst  ),
     .i_freeNext1  ( w_freeF_SEND_DATA  ),
     .i_freeNext2  ( w_freeF_GET_DATA  ),
@@ -100,7 +97,7 @@ cSelSplit5_streamInst u_cSelSplit5_streamInst(
     .valid2       ( w_valid2_streamInst       ),
     .valid3       ( w_valid3_streamInst       ),
     .valid4       ( w_valid4_streamInst       ),
-    .o_free       ( w_freeFAnalyzeInst       ),
+    .o_free       ( w_free_analyzeInst       ),
     .o_driveNext0 ( w_drive2_SEND_Inst ),
     .o_driveNext1 ( w_drive2_SEND_DATA ),
     .o_driveNext2 ( w_drive2_GET_DATA ),
@@ -109,6 +106,275 @@ cSelSplit5_streamInst u_cSelSplit5_streamInst(
     .rst          ( rst          )
 );
 
+//=========================================================================================================================================//
+// MEM_SEND_INST
+wire w_fire_MEM_SEND_Inst_FirstShake;
+cFifo1_user MEM_SEND_INST_fifo0(
+    .i_drive     ( w_drive2_SEND_Inst     ),
+    .o_free      ( w_freeF_SEND_Inst      ),
+    .o_driveNext ( w_driveF_MEM_SEND_Inst_FirstShake ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+
+
+// read calculate inst
+wire w_drive_readCalcInst;
+wire w_free_readCalcInst;
+cFifo1_user MEM_SEND_INST_fifo1(
+    .i_drive     (      ),
+    .o_free      (       ),
+    .o_driveNext ( w_drive_readCalcInst ),
+    .i_freeNext  ( w_free_readCalcInst ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+
+// send claculate inst to TPU
+
+cFifo1_user MEM_SEND_INST_fifo2(
+    .i_drive     (      ),
+    .o_free      (       ),
+    .o_driveNext ( w_driveF_calcInst_to_TPU ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+
+
+
+
+//=========================================================================================================================================//
+
+
+//MEM_SEND_DATA
+// send stream inst to TPU
+wire w_driveF_MEM_SEND_DATA_FirstShake;
+cFifo1_user MEM_SEND_DATA_fifo0(
+    .i_drive     ( w_drive2_SEND_DATA     ),
+    .o_free      ( w_freeF_SEND_DATA      ),
+    .o_driveNext ( w_driveF_MEM_SEND_DATA_FirstShake ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+// read matrix info 
+wire w_drive2Read_matrixInfo;
+cFifo1_user MEM_SEND_DATA_fifo1(
+    .i_drive     (      ),
+    .o_free      (       ),
+    .o_driveNext ( w_drive2Read_matrixInfo ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+
+//send matrix info to TPU
+wire w_driveF_matrixInfo_to_TPU;
+cFifo1_user MEM_SEND_DATA_fifo2(
+    .i_drive     (      ),
+    .o_free      (       ),
+    .o_driveNext ( w_driveF_matrixInfo_to_TPU ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+
+
+
+//=================================================================================================================//========================//
+
+// MEM_GET_DATA
+wire w_fire_MEM_GET_DATA_FirstShake;
+cFifo1_user MEM_GET_DATA_fifo0(
+    .i_drive     ( w_drive2_GET_DATA     ),
+    .o_free      ( w_freeF_GET_DATA      ),
+    .o_driveNext ( w_driveF_MEM_GET_DATA_FirstShake ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+// read matrix id MEMGETDATA
+wire w_drive2Read_matrixID_MEMGETDATA;
+cFifo1_user MEM_GET_DATA_fifo1(
+    .i_drive     (      ),
+    .o_free      (       ),
+    .o_driveNext ( w_drive2Read_matrixID_MEMGETDATA ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+//send matrix id to TPU MEMGETDATA
+wire w_driveF_matrixID_to_TPU_MEMGETDATA;
+cFifo1_user MEM_GET_DATA_fifo2(
+    .i_drive     (      ),
+    .o_free      (       ),
+    .o_driveNext ( w_driveF_matrixID_to_TPU_MEMGETDATA ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+
+//==========================================================================================================================================//  
+
+// MEM_FREE
+wire w_fire_MEM_FREE_FirstShake;
+cFifo1_user MEM_FREE_fifo0(
+    .i_drive     ( w_drive2_FREE     ),
+    .o_free      ( w_freeF_FREE      ),
+    .o_driveNext ( w_driveF_MEM_FREE_FirstShake ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+//read matrix id MEMFREE
+wire w_drive2Read_matrixID_MEMFREE;
+cFifo1_user MEM_FREE_fifo1(
+    .i_drive     (      ),
+    .o_free      (       ),
+    .o_driveNext ( w_drive2Read_matrixID_MEMFREE ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+
+wire w_driveF_matrixID_to_TPU_MEMFREE;
+cFifo1_user MEM_FREE_fifo2(
+    .i_drive     (      ),
+    .o_free      (       ),
+    .o_driveNext ( w_driveF_matrixID_to_TPU_MEMFREE ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+
+
+//=========================================================================================================================================//
+
+// MEM_GET_STATUS
+wire w_fire_MEM_GET_STATUS_FirstShake;
+cFifo1_user MEM_GET_STATUS_fifo0(
+    .i_drive     ( w_drive2_GET_STATUS     ),
+    .o_free      ( w_freeF_GET_STATUS      ),
+    .o_driveNext ( w_driveF_MEM_GET_STATUS_FirstShake ),
+    .i_freeNext  (  ),
+    .o_fire      (      ), 
+    .rst         ( rst         )
+);
+
+
+
+
+//============================================================================================================================================//
+
+
+streamInstReader u_streamInstReader(
+
+    .i_driveRead_0 ( w_drive_readStreamInst ),
+    .o_freeRead_0  ( w_free_readStreamInst  ),
+    .i_readAddr_0  (   ),
+    .o_driveRead_0 ( w_drive_streamInstReadDone  ),
+    .i_freeRead_0  ( w_free_streamInstReadDone  ),
+    .o_readData_0  (   ),
+
+    .i_driveRead_1 ( w_drive_readCalcInst ),
+    .o_freeRead_1  ( w_free_readCalcInst  ),
+    .i_readAddr_1  (   ),
+    .o_driveRead_1 (  ),
+    .i_freeRead_1  (   ),
+    .o_readData_1  (   ),
+    .i_driveRead_2 (  ),
+    .o_freeRead_2  (   ),
+    .i_readAddr_2  (   ),
+    .o_driveRead_2 (  ),
+    .i_freeRead_2  (   ),
+    .o_readData_2  (   ),
+    .i_driveRead_3 (  ),
+    .o_freeRead_3  (   ),
+    .i_readAddr_3  (   ),
+    .o_driveRead_3 (  ),
+    .i_freeRead_3  (   ),
+    .o_readData_3  (   ),
+    .i_driveRead_4 (  ),
+    .o_freeRead_4  (   ),
+    .i_readAddr_4  (   ),
+    .o_driveRead_4 (  ),
+    .i_freeRead_4  (   ),
+    .i_driveRead_5 (  ),
+    .i_readAddr_5  (   ),
+    .o_driveRead_5 (  ),
+    .i_freeRead_5  (   ),
+    .o_readData_5  (   ),
+    .i_driveRead_6 (  ),
+    .o_freeRead_6  (   ),
+    .i_readAddr_6  (   ),
+    .o_driveRead_6 (  ),
+    .i_freeRead_6  (   ),
+    .o_readData_6  (   ),
+    .i_driveRead_7 (  ),
+    .o_freeRead_7  (   ),
+    .i_readAddr_7  (   ),
+    .o_driveRead_7 (  ),
+    .i_freeRead_7  (   ),
+    .o_readData_7  (   ),
+    .i_driveRead_8 (  ),
+    .o_freeRead_8  (   ),
+    .i_readAddr_8  (   ),
+    .o_driveRead_8 (  ),
+    .i_freeRead_8  (   ),
+    .o_readData_8  (   ),
+    .RD_START      ( RD_START      ),
+    .RD_ADDR       ( RD_ADDR       ),
+    .RD_DATA       ( RD_DATA       ),
+    .RD_DONE       ( RD_DONE       ),
+    .rst           ( rst           )
+);
+
+
+
+
+//================================================================================================================================================//
+// mutex merge for drive and data and finish to TPU
+
+
+
+
+
+// TPU BACK
+wire w_fire_saveTPU_data;
+cFifo1_user MEM_GET_STATUS_fifo0(
+    .i_drive     ( i_driveFTPU     ),
+    .o_free      ( o_free2TPU      ),
+    .o_driveNext (  ),
+    .i_freeNext  (  ),
+    .o_fire      (  w_fire_saveTPU_data    ), 
+    .rst         ( rst         )
+);
+
+reg[128:0] TPU_data_finish_reg;
+always @(posedge w_fire_saveTPU_data or negedge rst) begin
+    if (!rst) begin
+         TPU_data_finish_reg<= 129'b0;
+    end else begin
+         TPU_data_finish_reg<= {i_finishFTPU, i_dataFTPU_128};
+    end
+end
+
+
+// select split for TPU drive and data and finish
 
 
 
